823d5d9bc2c822b6bd811ed2312d4eab478fa886
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-05-11 20:41:31 -0700

    checks if left arrow on Carousel will change the image to the corresponding image

--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -9,6 +9,13 @@ it('renders without crashing', function() {
     );
 });
 
+it('matches snapshot', function() {
+  const {asFragment} = render(
+    <Carousel photos={TEST_IMAGES} title="images for testing"/>
+    );
+  expect(asFragment()).toMatchSnapshot();
+});
+
 it("works when you click on the right arrow", function() {
   const { container } = render(
     <Carousel
@@ -36,3 +43,33 @@ it("works when you click on the right arrow", function() {
     container.querySelector('img[alt="testing image 2"]')
   ).toBeInTheDocument();
 });
+
+it("works when you click on the left arrow", function() {
+  const { container } = render(
+    <Carousel
+      photos={TEST_IMAGES}
+      title="images for testing"
+    />
+  );
+  
+  // move forward in the carousel
+  const rightArrow = container.querySelector(".bi-arrow-right-circle");
+  fireEvent.click(rightArrow);
+
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).toBeInTheDocument();
+
+  // move backward in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+})
